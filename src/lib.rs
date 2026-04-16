3ecff3517559512e6b6cb9138ecf20f6d7c62973
Samuel Åkesson <samak519@student.liu.se>
2026-04-16 10:42:09 +0200

    use libresprite reference

--- src/lib.rs
+++ src/lib.rs
@@ -15,24 +15,25 @@ pub mod helpers;
 pub use self::color::*;
 
 /*
-https://github.com/aseprite/aseprite/blob/master/docs/ase-file-specs.md
+https://github.com/LibreSprite/LibreSprite/blob/master/docs/files/ase.txt
 
 ASE files use Intel (little-endian) byte order.
 
-BYTE (u8): An 8-bit unsigned integer value
-WORD (u16): A 16-bit unsigned integer value
-SHORT (i16): A 16-bit signed integer value
-DWORD (u32): A 32-bit unsigned integer value
-LONG (i32): A 32-bit signed integer value
-FIXED (f32): A 32-bit fixed point (16.16) value
-BYTE[n] ([u8; n]): "n" bytes.
-STRING:
-    WORD: string length (number of bytes)
-    BYTE[length]: characters (in UTF-8) The '\0' character is not included.
-PIXEL: One pixel, depending on the image pixel format:
-    RGBA: BYTE[4], each pixel have 4 bytes in this order Red, Green, Blue, Alpha.
-    Grayscale: BYTE[2], each pixel have 2 bytes in the order Value, Alpha.
-    Indexed: BYTE, Each pixel uses 1 byte (the index).
+BYTE            An 8-bit unsigned integer value
+WORD            A 16-bit unsigned integer value
+DWORD           A 32-bit unsigned integer value
+LONG            A 32-bit signed integer value
+BYTE[n]         "n" bytes.
+RECT            Four LONGs (in the order: x-pos, y-pos, width, heigth)
+STRING          length=WORD (how many characters to read next)
+                string=BYTE[length]
+                The \0 character isn't included.
+PIXEL           Pixel format:
+                - In RGB images, each pixel have 4 bytes in
+                  this order Red, Green, Blue, Alpha.
+                - In Grayscale images, each pixel have 2 bytes
+                  in the order Value, Alpha.
+                - In Indexed images, each pixel uses 1 byte (the index).
 */
 
 #[derive(Debug)]
